adee46080eb0ca40957e042d8e944bb0b147c3b3
Perberos <perberos@gmail.com>
2015-01-26 15:03:30 -0300

    adding the code in pure state

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php // config.php :: Low-level app/database variables.
+
+$dbsettings = Array(
+        "server"        => "localhost",     // MySQL server name. (Default: localhost)
+        "user"          => "",              // MySQL username.
+        "pass"          => "",              // MySQL password.
+        "name"          => "",              // MySQL database name.
+        "prefix"        => "dk",            // Prefix for table names. (Default: dk)
+        "secretword"    => "");             // Secret word used when hashing information for cookies.
+
+?>
